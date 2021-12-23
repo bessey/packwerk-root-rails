70173d7b5996b281df402872264c3ddb9056288c
Matt Bessey <matt.bessey@immersivelabs.co.uk>
2021-12-23 01:47:06 +0000

    better docs of example repo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   def show
+    # This is fine because its a public root
     IdentityDomain.find_account(123)
+    # This is broken because the constant is not a public root as the package doesn't have one
     AutoloadDomain.testing
   end
 end
--- app/domains/identity_domain/private.rb
+++ app/domains/identity_domain/private.rb
@@ -1,3 +1,11 @@
 module IdentityDomain
   Account = Struct.new(:id)
+
+  def test
+    # I'm allowed to reference this even though its defined outside my package's root, because it does belong
+    # to my package in reality
+    IdentityDomain.find_account(321)
+    # But this fails the check
+    BrokenDomain.find_account(321)
+  end
 end
